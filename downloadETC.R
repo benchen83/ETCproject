23a70e0b861bb7a826130378fda1af06ad55c5f3
benchen83 <boy83227@gmail.com>
2017-09-19 12:40:24 +0800

    日期未自動化

--- downloadETC.R
+++ downloadETC.R
@@ -1,30 +1,38 @@
 library(rvest)
-url="http://tisvcloud.freeway.gov.tw/history/TDCS/M04A/20170917/"
-url=paste0(url,"00/")
+library(dplyr)
 
+#先建立個空data.frame
+dfurl<-data.frame(stringsAsFactors = FALSE)
+
+
+for(i in 1:24){
+hour<-c("00/","01/","02/","03/","04/","05/","06/","07/","08/","09/","10/","11/","12/","13/","14/","15/","16/","17/","18/","19/","20/","21/","22/","23/")
+
+url="http://tisvcloud.freeway.gov.tw/history/TDCS/M04A/20170816/"
+url=paste0(url,hour[i])
+#找出網頁內放檔案超聯結的位置
 html.page <- read_html(url)
 route <- html_nodes(html.page,"tr > td[valign=top] >a")
-
-hour<-c(00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23)
-
 target <- html_attrs(route)
 target <- sapply(target,"[[",1)
 target <- target[-1]
 
+#完整路徑
 download.url <- paste0(url,target)
 
-w <- data.frame(url=download.url,stringsAsFactors = FALSE) 
-    
-w["name"]<- local({substring(download.url,75,91)})
+#建立data.frame來儲存下載網址
+dfurl<- rbind(dfurl,data.frame(url=download.url,stringsAsFactors = FALSE)) 
+}
+
+#新增欄位放置檔案要取的名稱
+dfurl<- local({ 
+  dfurl %>%
+   mutate(name = substring(dfurl$url,75,91))        
+    })
+
+#下載檔案
+for(i in c(1:4896)){
+download.file(url=dfurl[i,1],dfurl[i,2],method = 'auto',mode = 'wb')}
 
 
-for(i in c(1:10)){
-download.file(url=w[i,1],w[i,2],method = 'auto',mode = 'wb')}
 
-download.file(url=w[1,1],destfile = substring(url,13,16),method = 'auto',mode = 'wb')
-
-?download.file
-
-
-seq(from=4,to = 26,by = 2)
-View(w)
